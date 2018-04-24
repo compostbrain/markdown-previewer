e703b238b4b9637b75b7e5deefe4da0f0bcd5bdb
compostbrain <joelcahalan@gmail.com>
2018-04-24 16:41:44 -0400

    Add README gif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import { Grid, Row } from 'react-bootstrap';
-import marked from 'marked';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Previewer from "./components/Previewer";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -20,70 +19,3 @@ class App extends Component {
     );
   }
 }
-
-class Output extends Component {
-	state = {value: ''};
-
-	markUp = e => {
-		return { __html: marked(this.state.value, { sanitize: true }) };
-	}
-
-  render() {
-
-		const style = {
-			"wordBreak": "break-all",
-			"wordWrap": "break-word"
-		};
-
-		return (
-			<div className="col-xs-6">
-				<h3>Output</h3>
-				<div style={style} dangerouslySetInnerHTML={this.markUp()}></div>
-			</div>
-		);
-	}
-};
-
-
-class Input extends Component {
-	state = {value: ''};
-
-	onChange = e => {
-		this.setState({ value: e.target.value });
-		this.refs.output.setState({ value: e.target.value });
-	}
-
-	render() {
-		const style = {
-			resize: "none"
-		};
-
-		return (
-			<div>
-				<div className="col-xs-6">
-					<h3>Input</h3>
-					<textarea rows="20" className="form-control" style={style} defaultValue={this.state.value} onChange={this.onChange}></textarea>
-				</div>
-				<Output ref="output"/>
-			</div>
-		);
-	}
-};
-
-
-class Previewer extends Component {
-	render() {
-
-		document.body.style.background = "#EEE";
-
-		return (
-      <Grid>
-        <Row className="show-grid">
-          <Input />
-        </Row>
-      </Grid>
-		);
-	}
-};
-
-export default App;
--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,91 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Row } from "react-bootstrap";
+import marked from "marked";
 
-class Input extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: ''
-		};
-	}
+export default class Previewer extends Component {
+  render() {
+    document.body.style.background = "#EEE";
+
+    return (
+      <Grid>
+        <Row className="show-grid">
+          <Input />
+        </Row>
+      </Grid>
+    );
+  }
 }
 
-	handleChange(event) {
-		this.setState({value: event.target.value});
-	}
+class Input extends Component {
+  state = { value: "" };
 
+  onChange = e => {
+    this.setState({ value: e.target.value });
+    this.refs.output.setState({ value: e.target.value });
+  };
 
-	render() {
-		return (
-			<div className={style.col-xs-6}>
-				<form action="">
-					<label htmlFor="">Enter Markdown
-						<textarea name="" id="" cols="30" rows="10"></textarea>
-					</label>
-				</form>
-			</div>
-		)
-	}
+  render() {
+    const style = {
+      resize: "none"
+    };
 
-class Previewer extends Component {
-	render() {
-		return (
-			div
-		)
-	}
-};
+    return (
+      <div>
+        <div className="col-xs-6">
+          <h3>Input</h3>
+          <textarea
+            rows="20"
+            className="form-control"
+            style={style}
+            defaultValue={this.state.value}
+            onChange={this.onChange}
+          />
+        </div>
+        <Output ref="output" />
+      </div>
+    );
+  }
+}
 
-export default Previewer;
-// class Output extends Component {
-// 	getInitialState: function () {
-// 		return { value: "" };
-// 	},
-//
-// 	markUp: function () {
-// 		return { __html: marked(this.state.value, { sanitize: true }) };
-// 	},
-//
-// 	render: function () {
-//
-// 		const style = {
-// 			"wordBreak": "break-all",
-// 			"wordWrap": "break-word"
-// 		};
-//
-// 		return (
-// 			<div className="col-xs-6">
-// 				<h3>Output</h3>
-// 				<div style={style} dangerouslySetInnerHTML={this.markUp()}></div>
-// 			</div>
-// 		);
-// 	}
-// };
-//
-//
-// class Input extends Component {
-// 	getInitialState: function () {
-// 		return { value: "" };
-// 	},
-//
-// 	onChange: function (e) {
-// 		this.setState({ value: e.target.value });
-// 		this.refs.output.setState({ value: e.target.value });
-// 	},
-//
-// 	render: function () {
-// 		const style = {
-// 			resize: "none"
-// 		};
-//
-// 		return (
-// 			<div>
-// 				<div className="col-xs-6">
-// 					<h3>Input</h3>
-// 					<textarea rows="20" className="form-control" style={style} defaultValue={this.state.value} onChange={this.onChange}></textarea>
-// 				</div>
-// 				<Output ref="output"/>
-// 			</div>
-// 		);
-// 	}
-// });
-//
-//
+class Output extends Component {
+  state = { value: "" };
+
+  markUp = e => {
+    return { __html: marked(this.state.value, { sanitize: true }) };
+  };
+
+  render() {
+    const style = {
+      wordBreak: "break-all",
+      wordWrap: "break-word"
+    };
+
+    return (
+      <div className="col-xs-6">
+        <h3>Output</h3>
+        <div style={style} dangerouslySetInnerHTML={this.markUp()} />
+      </div>
+    );
+  }
+}
